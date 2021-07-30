337f9ee3ef29aaa38700d938a1a478e0f4df951f
orientpine <orientpine@gmail.com>
2021-07-30 21:22:45 +0900

    Some message

--- main.py
+++ main.py
@@ -293,7 +293,7 @@ if st.button('Analyze'):
                     st.markdown('<p style="font-family:sans-serif; color:Black;font-size: 32px;"><strong>SARS-CoV-2</strong></p>', unsafe_allow_html=True)
                 st.markdown('<p style="font-family:sans-serif; color:Black;font-size: 25x;">Result:</p>', unsafe_allow_html=True)
                 st.markdown('<p style="font-family:sans-serif; color:Red; font-size: 32px;"><strong>POSITIVE</strong></p>', unsafe_allow_html=True)
-                st.markdown(f'<p style="font-family:sans-serif; color:Black;font-size: 18px;"><strong>DNA Concentration $$\approx$$ {list_concentration[label_before]}</strong></p>', unsafe_allow_html=True)
+                st.markdown(f'<p style="font-family:sans-serif; color:Black;font-size: 18px;"><strong>DNA Concentration ≈ {list_concentration[label_before]}</strong></p>', unsafe_allow_html=True)
 
             else:
                 if selected_option == 'SARS-CoV-2 variant':
@@ -302,7 +302,7 @@ if st.button('Analyze'):
                     st.markdown('<p style="font-family:sans-serif; color:Black;font-size: 32px;"><strong>SARS-CoV-2</strong></p>', unsafe_allow_html=True)
                 st.markdown('<p style="font-family:sans-serif; color:Black;font-size: 25px;">Result:</p>', unsafe_allow_html=True)
                 st.markdown('<p style="font-family:sans-serif; color:Blue; font-size: 32px;"><strong>NEGATIVE</strong></p>', unsafe_allow_html=True)
-                st.markdown(f'<p style="font-family:sans-serif; color:Black;font-size: 18px;"><strong>DNA Concentration $$\approx$$ {list_concentration[label_before]}</strong></p>', unsafe_allow_html=True)
+                st.markdown(f'<p style="font-family:sans-serif; color:Black;font-size: 18px;"><strong>DNA Concentration ≈ {list_concentration[label_before]}</strong></p>', unsafe_allow_html=True)
 
         except:
             st.markdown('Please __re-upload__ images')
